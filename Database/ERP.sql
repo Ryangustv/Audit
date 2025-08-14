347e302e1a50718fabbd08e1f19d000427045b37
R�AN GUSTAVO MARQUES FERREIRA <ryan.26985@aluno.pr.senac.br>
2025-08-13 21:52:59 -0300

    Inserindo sql

--- Database/ERP.sql
+++ Database/ERP.sql
@@ -1,2 +1,57 @@
-select *
-from produtos;
+CREATE TABLE IF NOT EXISTS produtos (
+    codigo VARCHAR(255),
+    descricao VARCHAR(255),
+    preco DECIMAL,
+    ecommerce BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS grades (
+    escala VARCHAR(255),
+    grade1 VARCHAR(255),
+    grade2 VARCHAR(255),
+    grade3 VARCHAR(255),
+    grade4 VARCHAR(255),
+    grade5 VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS estoques (
+    produto VARCHAR(255),
+    empresa VARCHAR(255),
+    quantidade INTEGER
+);
+
+
+INSERT INTO produtos (codigo, descricao, preco, ecommerce) VALUES
+('001', 'Camisa Esportiva', 49.90, TRUE),
+('002', 'Bola de Futebol', 79.90, TRUE),
+('003', 'Chuteira Profissional', 199.90, TRUE),
+('004', 'Calção Esportivo', 29.90, FALSE),
+('005', 'Meião de Futebol', 19.90, TRUE),
+('006', 'Luvas de Goleiro', 89.90, FALSE),
+('007', 'Garrafa Squeeze', 14.90, TRUE),
+('008', 'Coletes de Treino', 39.90, FALSE),
+('009', 'Apito para Árbitro', 9.90, TRUE),
+('010', 'Caneleira Profissional', 24.90, FALSE);
+
+INSERT INTO grades (escala, grade1, grade2, grade3, grade4, grade5) VALUES
+('ROUPA', 'PP', 'P', 'M', 'G', 'GG'),
+('TENIS', '38', '39', '40', '41', '42');
+
+INSERT INTO grades (escala, grade1) VALUES
+('UNICO', 'U');
+
+INSERT INTO estoques (produto, empresa, quantidade) VALUES
+('001PP', '01', 10),
+('001P', '01', 15),
+('001M', '01', 5),
+('001G', '01', 2),
+('001GG', '01', 1),
+('002U', '01', 12),
+('00338', '01', 3),
+('00339', '01', 4),
+('00340', '01', 4),
+('00341', '01', 3),
+('00342', '01', 1),
+('007U', '01', 2),
+('009U', '01', 5),
+('010U', '01', 6);
--- Database/HUB.sql
+++ Database/HUB.sql
@@ -1,21 +1,101 @@
-CREATE TABLE IF NOT EXISTS produtos (
+CREATE TABLE IF NOT EXISTS mpp_produto (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    codigo VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS mpp_produto_sku (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     codigo VARCHAR(255),
-    descricao VARCHAR(255),
-    preco DECIMAL,
-    ecommerce BOOLEAN
+    id_mpp_produto INT REFERENCES mpp_produto(id)
 );
 
-CREATE TABLE IF NOT EXISTS grades (
-    escala VARCHAR(255),
-    grade1 VARCHAR(255),
-    grade2 VARCHAR(255),
-    grade3 VARCHAR(255),
-    grade4 VARCHAR(255),
-    grade5 VARCHAR(255)
+CREATE TABLE IF NOT EXISTS mpp_armazem (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    descricao VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS estoques (
-    produto VARCHAR(255),
-    empresa VARCHAR(255),
-    quantidade INTEGER
+CREATE TABLE IF NOT EXISTS mpp_produto_sku_armazem (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    saldo INT,
+    id_mpp_produto_sku INT REFERENCES mpp_produto_sku(id),
+    id_mpp_armazem INT REFERENCES mpp_armazem(id)
 );
+
+CREATE TABLE IF NOT EXISTS mpp_entidade (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_entidade INT,
+    status VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS mpp_entidade_sinc (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_entidade INT REFERENCES mpp_entidade(id),
+    status VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS mpp_iee (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    tabela_erp_pk INT,
+    tabela_erp VARCHAR(255),
+    tabela_hub_pk INT,
+    tabela_hub VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS mpp_seller_produto (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    codigo VARCHAR(255),
+    id_mpp_produto INT REFERENCES mpp_produto(id)
+);
+
+CREATE TABLE IF NOT EXISTS mpp_seller_produto_sku (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    codigo VARCHAR(255),
+    id_mpp_seller_produto INT REFERENCES mpp_seller_produto(id),
+    id_mpp_produto_sku INT REFERENCES mpp_produto_sku(id)
+);
+
+INSERT INTO mpp_produto (codigo) VALUES
+('PROD001'),
+('PROD002'),
+('PROD003');
+
+INSERT INTO mpp_produto_sku (codigo, id_mpp_produto) VALUES
+('PROD001-SKU1', 1),
+('PROD001-SKU2', 1),
+('PROD002-SKU1', 2),
+('PROD003-SKU1', 3),
+('PROD003-SKU2', 3);
+
+INSERT INTO mpp_armazem (descricao) VALUES
+('Armazém Centro'),
+('Armazém Norte');
+
+INSERT INTO mpp_produto_sku_armazem (saldo, id_mpp_produto_sku, id_mpp_armazem) VALUES
+(100, 1, 1),
+(50, 2, 1),
+(200, 3, 2),
+(30, 4, 1),
+(60, 5, 2);
+
+INSERT INTO mpp_entidade (id_entidade, status) VALUES
+(101, 'Ativo'),
+(102, 'Inativo');
+
+INSERT INTO mpp_entidade_sinc (id_entidade, status) VALUES
+(1, 'Sincronizado'),
+(2, 'Pendente');
+
+
+INSERT INTO mpp_iee (tabela_erp_pk, tabela_erp, tabela_hub_pk, tabela_hub) VALUES
+(1, 'produtos', 101, 'mpp_produto'),
+(2, 'estoques', 102, 'mpp_produto_sku_armazem');
+
+
+INSERT INTO mpp_seller_produto (codigo, id_mpp_produto) VALUES
+('SELLER_PROD001', 1),
+('SELLER_PROD002', 2);
+
+INSERT INTO mpp_seller_produto_sku (codigo, id_mpp_seller_produto, id_mpp_produto_sku) VALUES
+('SELLER_PROD001-SKU1', 1, 1),
+('SELLER_PROD001-SKU2', 1, 2),
+('SELLER_PROD002-SKU1', 2, 3);
